0af223592be141b6a32161b2772d28bff6e6dece
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-09-17 20:59:49 -0300

    Agregada cámara a helicóptero

---
+++ Assets/Scripts/Transform/MirarHacia.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirarHacia : MonoBehaviour {
+
+    public Transform objetivo;
+	void Update () {
+        transform.LookAt(objetivo);
+	}
+}
