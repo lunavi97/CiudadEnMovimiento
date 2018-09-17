22594332adb67b13d3a06919901d5af280c6225c
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-09-17 19:48:26 -0300

    Agregadas propiedades de Vector3 arriba y abajo para movimiento

--- Assets/Scripts/Vector3/Vector3Movimiento.cs
+++ Assets/Scripts/Vector3/Vector3Movimiento.cs
@@ -8,6 +8,8 @@ public class Vector3Movimiento : MonoBehaviour {
     public Vector3 atras = Vector3.back;
     public Vector3 izquierda = Vector3.left;
     public Vector3 derecha = Vector3.right;
+    public Vector3 arriba = Vector3.up;
+    public Vector3 abajo = Vector3.down;
 	void Update () {
 	}
 }
