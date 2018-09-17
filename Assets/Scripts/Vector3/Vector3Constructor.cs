80862651371ed463f6d2ec68ff373cbf06d5b7d2
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-09-17 19:24:06 -0300

    Creado vector e incorporado a coche para trasladar su posición

---
+++ Assets/Scripts/Vector3/Vector3Constructor.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vector3Constructor : MonoBehaviour {
+
+    public Vector3 nuevoVector = new Vector3(0, 0, 0);
+    void Update ()
+    {
+        transform.position = nuevoVector;
+    }
+}
