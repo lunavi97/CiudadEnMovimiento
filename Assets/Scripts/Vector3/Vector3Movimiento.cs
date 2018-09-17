53e5c8c4a257a4c1b3d7f07f2a75ccc1b5c07feb
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-09-17 19:42:31 -0300

    Movidas cámaras hacia adelante y hacia atrás

---
+++ Assets/Scripts/Vector3/Vector3Movimiento.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vector3Movimiento : MonoBehaviour {
+
+    public Vector3 adelante = Vector3.forward;
+    public Vector3 atras = Vector3.back;
+    private bool irAdelante = true;
+	void Update () {
+        if (irAdelante)
+        {
+            if (transform.position.z < 275)
+                transform.position += adelante;
+            else irAdelante = false;
+        }
+
+        else if (transform.position.z > -40)
+        {
+            transform.position += atras;
+        }
+
+        else irAdelante = true;
+	}
+}
