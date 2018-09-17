8312a33e6886af8a94d34874aafae5720db58760
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-09-17 20:42:11 -0300

    Implementado método para trasladar objetos

---
+++ Assets/Scripts/Transform/TrasladarObjeto.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrasladarObjeto : MonoBehaviour {
+
+    public float metrosSeg = 1;
+	void Update () {
+        transform.Translate(0, 0, metrosSeg * Time.deltaTime);
+	}
+}
