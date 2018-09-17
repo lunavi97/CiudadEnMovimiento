9b6645b152c61e63ba7252d23f440431ac4489e4
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-09-17 19:03:10 -0300

    Agregado tiempo de cada fotograma

---
+++ Assets/Scripts/Time/TiempoDelta.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiempoDelta : MonoBehaviour {
+
+    public float seg;
+	void Update () {
+        seg += Time.deltaTime;
+	}
+}
